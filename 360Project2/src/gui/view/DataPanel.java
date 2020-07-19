a7a5c0949b401067065d6486c869a588dd80d6c3
huynh201 <duyenhuynh201@gmail.com>
2020-07-19 14:14:16 -0700

    DataPanel updated. still need listeners.

--- 360Project2/src/gui/view/DataPanel.java
+++ 360Project2/src/gui/view/DataPanel.java
@@ -1,6 +1,7 @@
 package gui.view;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.text.DecimalFormat;
 
@@ -12,8 +13,10 @@ public class DataPanel extends JPanel {
     private static final Dimension BOARD_SIZE = new Dimension(700, 500);
     private static final Font dataFontSmall = new Font("Courier New", Font.BOLD, 16);
     private static final Font dataFontBig = new Font("Courier New", Font.BOLD, 30);
+    private static final EmptyBorder border = new EmptyBorder(5, 20, 5, 20);
     //Display values with decimal format of 1 decimal place.
     private DecimalFormat df = new DecimalFormat("0.0");
+
 
     private JLabel myTemp;
     private JLabel myHum;
@@ -50,27 +53,53 @@ public class DataPanel extends JPanel {
         myUV.setFont(dataFontBig);
         myChill.setFont(dataFontBig);
 
+        updateDataVals();
+
         JPanel tempPanel = new JPanel(new BorderLayout());
         tempPanel.add(tempLabel, BorderLayout.NORTH);
-        tempLabel.add(myTemp, BorderLayout.SOUTH);
+        tempPanel.add(myTemp, BorderLayout.SOUTH);
+        tempPanel.setBorder(border);
 
         JPanel humPanel = new JPanel(new BorderLayout());
         humPanel.add(humLabel, BorderLayout.NORTH);
         humPanel.add(myHum, BorderLayout.SOUTH);
+        humPanel.setBorder(border);
 
         JPanel uVPanel = new JPanel(new BorderLayout());
         uVPanel.add(uVLabel, BorderLayout.NORTH);
         uVPanel.add(myUV, BorderLayout.SOUTH);
+        uVPanel.setBorder(border);
 
         JPanel chillPanel = new JPanel(new BorderLayout());
         chillPanel.add(chillLabel, BorderLayout.NORTH);
         chillPanel.add(myChill, BorderLayout.SOUTH);
+        chillPanel.setBorder(border);
 
         this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
         gbc.insets = new Insets(0, 0, 50, 0);
-
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        this.add(tempPanel, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        this.add(humPanel, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        this.add(uVPanel, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        this.add(chillPanel, gbc);
 
     }
+
+    private void updateDataVals() {
+        double exampleData = 30;
+        myTemp.setText(df.format(exampleData));
+        myHum.setText(df.format(exampleData));
+        myUV.setText(df.format(exampleData));
+        myChill.setText(df.format(exampleData));
+    }
 }
+
