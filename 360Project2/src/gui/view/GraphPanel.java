1aededa3230d1bccb1e7bb4edcb6010cc26caf47
knguye23 <nguye.khue23@gmail.com>
2020-07-26 13:09:24 -0700

    added tick marks, adjusted panel size. Visually not ready yet

--- 360Project2/src/gui/view/GraphPanel.java
+++ 360Project2/src/gui/view/GraphPanel.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import gui.model.Connect;
@@ -26,13 +27,20 @@ import gui.model.Connect;
 public class GraphPanel extends JPanel implements Connect {
     
     /** GraphPanel's size. */
-    private static final Dimension PANEL_SIZE = new Dimension(500, 450);
+    private static final Dimension PANEL_SIZE = new Dimension(450, 400);
     
+    /** Font for lettered labels. */
+    private static final Font FONT = new Font("Avenir", Font.PLAIN ,8);
+    
+    /** Padding for whole GraphPannel. */
     private static final int PADDING = 15;
     
+    /** Padding for all labels. */
     private static final int LABEL_PADDING = 20;
     
-    private static final Font FONT = new Font("Avenir", Font.PLAIN ,8);
+    /** Tick width if y tick marks, tick height if x tick marks. */
+    private static final int TICK = 6;
+    
     
     /** Contains graph data with 20 values. */
     private List<Double> myData = new ArrayList<>(20);
@@ -40,18 +48,34 @@ public class GraphPanel extends JPanel implements Connect {
     /** Translated data to points. */
     private List<Point> myDataPoints = new ArrayList<>();
     
+    /* Min data value. */
     private double myMin = 0;
     
+    /* Max data value. */
     private double myMax = 0;
+    
+    /** Graph width. */
+    private int myGraphWidth;
+    
+    /** Graph height. */
+    private int myGraphHeight;
     
     /** Flag of graph's current data type being sent in. Here for future implementation. */
     private String myFlag;
     
+    
     /** Constructor that sets up components. */
     public GraphPanel() {
         super();
+        
+        for (int i = 0; i < 20; i++) {
+            myData.add(0.0);
+        }
+        
         setUpPanel();
-        //this.setBackground(Color.black); //test size
+        
+        
+        this.setBackground(Color.black); //test size
         addLabels();
         
     }
@@ -61,7 +85,10 @@ public class GraphPanel extends JPanel implements Connect {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-       
+        
+        myGraphWidth = this.getWidth() - 2 * PADDING - LABEL_PADDING;
+        myGraphHeight = this.getHeight() - 3 * LABEL_PADDING - 3 * PADDING;
+        
         createPoints(g2);
         createGrid(g2);
         createAxes(g2);
@@ -71,10 +98,9 @@ public class GraphPanel extends JPanel implements Connect {
     }
     
     private void createPoints(Graphics2D g2) { 
-        double xScale = (this.getWidth() - 2 * PADDING - LABEL_PADDING) / (myData.size() - 1);
-        double yScale = (this.getHeight() - 3 * PADDING - 3 * LABEL_PADDING) / 50; 
-        //50 is just an arbitrary max value 
-        
+        double xScale = (myGraphWidth) / (myData.size() - 1);
+        double yScale = (myGraphHeight) / 45; //45 is just an arbitrary max value 
+         
         for (int i = 0; i < myData.size(); i++) {
             int x = (int) (i * xScale + PADDING + LABEL_PADDING);
             int y = (int) (i * yScale + PADDING + LABEL_PADDING);
@@ -86,7 +112,8 @@ public class GraphPanel extends JPanel implements Connect {
         
     }
     
-    private void createAxes(Graphics2D g2) { 
+    private void createAxes(Graphics2D g2) {
+        g2.setColor(Color.white);
         //x axis
         g2.drawLine(PADDING + LABEL_PADDING, this.getHeight() - 2*PADDING - 2*LABEL_PADDING, 
                 this.getWidth() - PADDING, this.getHeight() - 2*PADDING - 2*LABEL_PADDING);
@@ -94,7 +121,30 @@ public class GraphPanel extends JPanel implements Connect {
         g2.drawLine(PADDING + LABEL_PADDING, this.getHeight() - 2*PADDING - 2*LABEL_PADDING, 
                 PADDING + LABEL_PADDING, PADDING + LABEL_PADDING);
         
+        g2.drawLine(PADDING, this.getHeight() - PADDING, PADDING, PADDING); //see padding
+        g2.drawLine(PADDING, this.getHeight() - PADDING, this.getWidth()-PADDING, this.getHeight()-PADDING); //padding
+        g2.drawLine(PADDING, this.getHeight()-PADDING-LABEL_PADDING, this.getWidth()-PADDING, this.getHeight()-PADDING-LABEL_PADDING); //label padding
         
+        
+        // x tick marks
+        for (int i = 0; i < myData.size(); i++) {
+            int x1 = PADDING + LABEL_PADDING + i*myGraphWidth/(myData.size()-1); //19 blocks but 20 ticks
+            int x2 = x1;
+            int y1 = this.getHeight() - 2*PADDING - 2*LABEL_PADDING;
+            int y2 = y1 + TICK;
+            g2.drawLine(x1, y1, x2, y2);
+            
+            
+       
+        }
+        // y tick marks
+        for (int i = 0; i < myData.size()* 59 / 80 + 1; i++) { //the graph has a 59:80 ratio
+            int x1 = PADDING + LABEL_PADDING; //19 blocks but 20 ticks
+            int x2 = x1 - TICK;
+            int y1 = this.getHeight() - 2*PADDING - 2*LABEL_PADDING - i*myGraphHeight/(myData.size()* 59 / 80);
+            int y2 = y1;
+            g2.drawLine(x1, y1, x2, y2);
+        }
     }
     
     private void connectPoints(Graphics2D g2) { 
@@ -142,7 +192,7 @@ public class GraphPanel extends JPanel implements Connect {
     public void changeDisplay(String data, String value) {
         myFlag = data;
         try {
-            String[] split = value.split(",");
+            String[] split = value.split(", ");
             for (int i = 0; i < split.length; i++) {
                 myData.set(i, Double.parseDouble(split[i]));
             }
