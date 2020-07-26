d55201ab2fd802ea4da430cd43c95d0f9d08e27b
knguye23 <nguye.khue23@gmail.com>
2020-07-25 22:41:57 -0700

    started on paintComponenent and setUpPanel

--- 360Project2/src/gui/view/GraphPanel.java
+++ 360Project2/src/gui/view/GraphPanel.java
@@ -1,8 +1,36 @@
 package gui.view;
 
-import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.JPanel;
 
+/**
+ * 
+ *
+ * @author Khue Nguyen
+ * @version Jul 25, 2020
+ */
+@SuppressWarnings("serial")
 public class GraphPanel extends JPanel {
-
+    
+    private static final Dimension PANEL_SIZE = new Dimension(300, 250);
+    /** Constructor that sets up components. */
+    public GraphPanel() {
+        super();
+        
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        
+    }
+    
+    /** Sets up panel properties. */
+    private void setUpPanel() {
+       this.setPreferredSize(PANEL_SIZE); 
+    }
 }
