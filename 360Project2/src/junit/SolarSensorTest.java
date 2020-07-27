a900e3c27c063ba9bfb4cd2489aa833705a6a735
skim-93 <68123310+skim-93@users.noreply.github.com>
2020-07-27 13:34:04 -0700

    Update SolarSensorTest.java

--- 360Project2/src/junit/SolarSensorTest.java
+++ 360Project2/src/junit/SolarSensorTest.java
@@ -7,26 +7,39 @@ import org.junit.jupiter.api.Test;
 
 import sensors.SolarSensor;
 
+
+/**
+ * Junit testing for Sensors
+ *
+ * @author Kim Seungku
+ * @version Jul 26, 2020
+ */
 class SolarSensorTest {
 
 	SolarSensor SStest = new SolarSensor();
 	
+	/**test toString method*/
 	@Test
 	void testToString() {
 		assertEquals(SStest.toString(), "Solar Radiation Sensor");
 	}
 	
+	/**test data of the sensor*/
 	@Test
 	void testUpdateData() {
 		for(int i = 0; i < 100000; i ++) {
+			//check if data is within the range
 			assertTrue(0.00 <= Double.valueOf(SStest.getDataOne()));
 			assertNull(SStest.getDataTwo());
+			//update the data
 			SStest.updateData();
+			//check again if data is within the range
 			assertTrue(0.00 <= Double.valueOf(SStest.getDataOne()));
 			assertNull(SStest.getDataTwo());
 		}
 	}
 	
+	/**test running method*/
 	@Test
 	void testRun() throws InterruptedException {
 		Thread thread = new Thread(SStest);
