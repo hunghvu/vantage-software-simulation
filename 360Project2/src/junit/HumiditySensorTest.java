31124ecfc7f361f12f01a6feb9ba675aa9a51d46
skim-93 <68123310+skim-93@users.noreply.github.com>
2020-07-27 13:32:20 -0700

    Update HumiditySensorTest.java

--- 360Project2/src/junit/HumiditySensorTest.java
+++ 360Project2/src/junit/HumiditySensorTest.java
@@ -6,26 +6,39 @@ import org.junit.jupiter.api.Test;
 
 import sensors.HumiditySensor;
 
+
+/**
+ * Junit testing for Sensors
+ *
+ * @author Kim Seungku
+ * @version Jul 26, 2020
+ */
 class HumiditySensorTest {
 
 	HumiditySensor HStest = new HumiditySensor();
 	
+	/**test toString method*/
 	@Test
 	void testToString() {
 		assertEquals(HStest.toString(), "Humidity Sensor");
 	}
 	
+	/**test data of the sensor*/
 	@Test
 	void testUpdateData() {
 		for(int i = 0; i < 100000; i ++) {
+			//check if data is within the range
 			assertTrue(0.00 <= Double.valueOf(HStest.getDataOne()) && Double.valueOf(HStest.getDataOne()) <= 100.00);
 			assertTrue(40.00 <= Double.valueOf(HStest.getDataTwo()) && Double.valueOf(HStest.getDataTwo()) <= 50.00);
+			//update the data
 			HStest.updateData();
+			//check again if data is within the range
 			assertTrue(0.00 <= Double.valueOf(HStest.getDataOne()) && Double.valueOf(HStest.getDataOne()) <= 100.00);
 			assertTrue(40.00 <= Double.valueOf(HStest.getDataTwo()) && Double.valueOf(HStest.getDataTwo()) <= 50.00);
 		}
 	}
 	
+	/**test running method*/
 	@Test
 	void testRun() throws InterruptedException {
 		Thread thread = new Thread(HStest);
