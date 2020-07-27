eb1015d1e68dabd58346b8d7c21bb29b3637f4f2
hunghvu <62678592+hunghvu@users.noreply.github.com>
2020-07-27 16:06:50 -0700

    Partially refactoring junit test

--- 360Project2/src/junit/BarometricSensorTest.java
+++ 360Project2/src/junit/BarometricSensorTest.java
@@ -1,52 +1,62 @@
 
-
 package junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import sensors.BarometricSensor;
 
 /**
- * Junit testing for Sensors
+ * Junit testing for Sensors.
  *
  * @author Kim Seungku
  * @version Jul 26, 2020
  */
 
 class BarometricSensorTest {
+  /** Object for setting up purpose **/
+  private BarometricSensor baroTest = new BarometricSensor();
 
-	BarometricSensor BStest = new BarometricSensor();
-	
-	/**test toString method*/
-	@Test
-	void testToString() {
-		assertEquals(BStest.toString(), "Barometric Sensor");
-	}
-	
-	/**test data of the sensor*/
-	@Test
-	void testUpdateData() {
-		for(int i = 0; i < 10000; i ++) {
-			//check if data is within the range
-			assertTrue(16.00 <= Double.valueOf(BStest.getDataOne()) && Double.valueOf(BStest.getDataOne()) <= 32.50);
-			assertTrue(BStest.getDataTwo() == "Rapidly falling" || BStest.getDataTwo() == "Slowly falling" 
-					|| BStest.getDataTwo() == "Steady" || BStest.getDataTwo() == "Slowly rising" || BStest.getDataTwo() == "Rapidly rising");
-			//update the data
-			BStest.updateData();
-			//check again if data is within the range
-			assertTrue(16.00 <= Double.valueOf(BStest.getDataOne()) && Double.valueOf(BStest.getDataOne()) <= 32.50);
-			assertTrue(BStest.getDataTwo() == "Rapidly falling" || BStest.getDataTwo() == "Slowly falling" 
-					|| BStest.getDataTwo() == "Steady" || BStest.getDataTwo() == "Slowly rising" || BStest.getDataTwo() == "Rapidly rising");
-		}
-	}
-	
-	/**test running method*/
-	@Test
-	void testRun() throws InterruptedException {
-		Thread thread = new Thread(BStest);
-		thread.start(); 
-		Thread.sleep(3000L);
-	}
+  /** test toString method **/
+  @Test
+  void testToString() {
+    assertEquals(baroTest.toString(), "Barometric Sensor");
+  }
+
+  /** test data of the sensor. **/
+  @Test
+  void testUpdateData() {
+    for (int i = 0; i < 10_000; i++) {
+      // check if data is within the range
+      assertTrue(16.00 <= Double.valueOf(baroTest.getDataOne()) 
+          && Double.valueOf(baroTest.getDataOne()) <= 32.50);
+      assertTrue("Rapidly falling".equals(baroTest.getDataTwo()) 
+          || "Slowly falling".equals(baroTest.getDataTwo())
+          || "Steady".equals(baroTest.getDataTwo()) 
+          || "Slowly rising".equals(baroTest.getDataTwo())
+          || "Rapidly rising".equals(baroTest.getDataTwo()));
+      // update the data
+      baroTest.updateData();
+      // check again if data is within the range
+      assertTrue(16.00 <= Double.valueOf(baroTest.getDataOne()) 
+          && Double.valueOf(baroTest.getDataOne()) <= 32.50);
+      assertTrue("Rapidly falling".equals(baroTest.getDataTwo())
+          || "Slowly falling".equals(baroTest.getDataTwo())
+          || "Steady".equals(baroTest.getDataTwo()) 
+          || "Slowly rising".equals(baroTest.getDataTwo())
+          || "Rapidly rising".equals(baroTest.getDataTwo()));
+    }
+  }
+
+  /** test running method */
+  @Test
+  void testRun() throws InterruptedException {
+    final Thread thread = new Thread(baroTest);
+    thread.start();
+    Thread.sleep(3000L);
+  }
 }
