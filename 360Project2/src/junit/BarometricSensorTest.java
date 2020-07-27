ecb489ed9dce449ef1cc14d262078da93d461836
skim-93 <68123310+skim-93@users.noreply.github.com>
2020-07-27 13:31:14 -0700

    Update BarometricSensorTest.java
    Java Doc

--- 360Project2/src/junit/BarometricSensorTest.java
+++ 360Project2/src/junit/BarometricSensorTest.java
@@ -1,3 +1,5 @@
+
+
 package junit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,28 +8,41 @@ import org.junit.jupiter.api.Test;
 
 import sensors.BarometricSensor;
 
+/**
+ * Junit testing for Sensors
+ *
+ * @author Kim Seungku
+ * @version Jul 26, 2020
+ */
+
 class BarometricSensorTest {
 
 	BarometricSensor BStest = new BarometricSensor();
 	
+	/**test toString method*/
 	@Test
 	void testToString() {
 		assertEquals(BStest.toString(), "Barometric Sensor");
 	}
 	
+	/**test data of the sensor*/
 	@Test
 	void testUpdateData() {
 		for(int i = 0; i < 10000; i ++) {
+			//check if data is within the range
 			assertTrue(16.00 <= Double.valueOf(BStest.getDataOne()) && Double.valueOf(BStest.getDataOne()) <= 32.50);
 			assertTrue(BStest.getDataTwo() == "Rapidly falling" || BStest.getDataTwo() == "Slowly falling" 
 					|| BStest.getDataTwo() == "Steady" || BStest.getDataTwo() == "Slowly rising" || BStest.getDataTwo() == "Rapidly rising");
+			//update the data
 			BStest.updateData();
+			//check again if data is within the range
 			assertTrue(16.00 <= Double.valueOf(BStest.getDataOne()) && Double.valueOf(BStest.getDataOne()) <= 32.50);
 			assertTrue(BStest.getDataTwo() == "Rapidly falling" || BStest.getDataTwo() == "Slowly falling" 
 					|| BStest.getDataTwo() == "Steady" || BStest.getDataTwo() == "Slowly rising" || BStest.getDataTwo() == "Rapidly rising");
 		}
 	}
 	
+	/**test running method*/
 	@Test
 	void testRun() throws InterruptedException {
 		Thread thread = new Thread(BStest);
