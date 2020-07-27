504396998174fea66aaa4fb39b24200334b4150c
skim-93 <68123310+skim-93@users.noreply.github.com>
2020-07-27 13:35:49 -0700

    Update WindDirectionSensorTest.java

--- 360Project2/src/junit/WindDirectionSensorTest.java
+++ 360Project2/src/junit/WindDirectionSensorTest.java
@@ -7,24 +7,36 @@ import org.junit.jupiter.api.Test;
 import sensors.TemperatureSensor;
 import sensors.WindDirectionSensor;
 
+
+/**
+ * Junit testing for Sensors
+ *
+ * @author Kim Seungku
+ * @version Jul 26, 2020
+ */
 class WindDirectionSensorTest {
 
 	WindDirectionSensor WDtest = new WindDirectionSensor();
 
+	/**test toString method*/
 	@Test
 	void testToString() {
 		assertEquals(WDtest.toString(), "Wind Direction Sensor");
 	}
 
+	/**test data of the sensor*/
 	@Test
 	void testUpdateData() {
 		for(int i = 0; i < 100000; i ++) {
+			//check if data is within the range
 			assertTrue(0 <= Double.valueOf(WDtest.getDataOne()));
 			assertTrue(WDtest.getDataTwo() == "East" || WDtest.getDataTwo() == "SouthEast" ||
 					WDtest.getDataTwo() == "South" || WDtest.getDataTwo() == "SouthWest" ||
 					WDtest.getDataTwo() == "West" || WDtest.getDataTwo() == "NorthWest" ||
 					WDtest.getDataTwo() == "North" || WDtest.getDataTwo() == "NorthEast");
+			//update the data
 			WDtest.updateData();
+			//check again if data is within the range
 			assertTrue(0 <= Double.valueOf(WDtest.getDataOne()));
 			assertTrue(WDtest.getDataTwo() == "East" || WDtest.getDataTwo() == "SouthEast" ||
 					WDtest.getDataTwo() == "South" || WDtest.getDataTwo() == "SouthWest" ||
@@ -33,6 +45,7 @@ class WindDirectionSensorTest {
 		}
 	}
 
+	/**test running method*/
 	@Test
 	void testRun() throws InterruptedException {
 		Thread thread = new Thread(WDtest);
