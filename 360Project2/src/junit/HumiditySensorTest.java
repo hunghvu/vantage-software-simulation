03e54f0d30becaa769e8c4413b1ed63d57866136
skim-93 <68123310+skim-93@users.noreply.github.com>
2020-07-27 17:03:04 -0700

    Update HumiditySensorTest.java
    added assertion comments

--- 360Project2/src/junit/HumiditySensorTest.java
+++ 360Project2/src/junit/HumiditySensorTest.java
@@ -20,7 +20,8 @@ class HumiditySensorTest {
 	/**test toString method*/
 	@Test
 	void testToString() {
-		assertEquals(HStest.toString(), "Humidity Sensor");
+		assertEquals("Humidity sensor is not able to call right toString method",
+			     HStest.toString(), "Humidity Sensor");
 	}
 	
 	/**test data of the sensor*/
@@ -28,13 +29,17 @@ class HumiditySensorTest {
 	void testUpdateData() {
 		for(int i = 0; i < 100000; i ++) {
 			//check if data is within the range
-			assertTrue(0.00 <= Double.valueOf(HStest.getDataOne()) && Double.valueOf(HStest.getDataOne()) <= 100.00);
-			assertTrue(40.00 <= Double.valueOf(HStest.getDataTwo()) && Double.valueOf(HStest.getDataTwo()) <= 50.00);
+			assertTrue("Humidity out is not within the range of the sensor",
+				   0.00 <= Double.valueOf(HStest.getDataOne()) && Double.valueOf(HStest.getDataOne()) <= 100.00);
+			assertTrue("Humidity in is not within the range of the sensor",
+				   40.00 <= Double.valueOf(HStest.getDataTwo()) && Double.valueOf(HStest.getDataTwo()) <= 50.00);
 			//update the data
 			HStest.updateData();
 			//check again if data is within the range
-			assertTrue(0.00 <= Double.valueOf(HStest.getDataOne()) && Double.valueOf(HStest.getDataOne()) <= 100.00);
-			assertTrue(40.00 <= Double.valueOf(HStest.getDataTwo()) && Double.valueOf(HStest.getDataTwo()) <= 50.00);
+			assertTrue("Humidity out is not within the range of the sensor",
+				0.00 <= Double.valueOf(HStest.getDataOne()) && Double.valueOf(HStest.getDataOne()) <= 100.00);
+			assertTrue("Humidity in is not within the range of the sensor",
+				   40.00 <= Double.valueOf(HStest.getDataTwo()) && Double.valueOf(HStest.getDataTwo()) <= 50.00);
 		}
 	}
 	
