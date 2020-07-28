e5ccc82e8c42716559df8eb508785669c65b5135
skim-93 <68123310+skim-93@users.noreply.github.com>
2020-07-27 17:04:45 -0700

    Update BarometricSensorTest.java
    updated assertion comment

--- 360Project2/src/junit/BarometricSensorTest.java
+++ 360Project2/src/junit/BarometricSensorTest.java
@@ -24,8 +24,8 @@ class BarometricSensorTest {
   /** test toString method **/
   @Test
   void testToString() {
-    assertEquals(baroTest.toString(), "Barometric Sensor",
-                 "There is something wrong with Barometric Sensor toString method");
+    assertEquals("There is something wrong with Barometric Sensor toString method",
+                 baroTest.toString(), "Barometric Sensor");
   }
 
   /** test data of the sensor. **/
@@ -33,23 +33,27 @@ class BarometricSensorTest {
   void testUpdateData() {
     for (int i = 0; i < 10_000; i++) {
       // check if data is within the range
-      assertTrue(16.00 <= Double.valueOf(baroTest.getDataOne()) 
-          && Double.valueOf(baroTest.getDataOne()) <= 32.50,"the Barometric data is not within the range");
-      assertTrue("Rapidly falling".equals(baroTest.getDataTwo()) 
+      assertTrue("the Barometric data is not within the range",
+                 16.00 <= Double.valueOf(baroTest.getDataOne()) 
+          && Double.valueOf(baroTest.getDataOne()) <= 32.50);
+      assertTrue("Barometric sensor cannot determines the rate of falling",
+                 "Rapidly falling".equals(baroTest.getDataTwo()) 
           || "Slowly falling".equals(baroTest.getDataTwo())
           || "Steady".equals(baroTest.getDataTwo()) 
           || "Slowly rising".equals(baroTest.getDataTwo())
-          || "Rapidly rising".equals(baroTest.getDataTwo()), "Barometric sensor cannot determines the rate of falling");
+          || "Rapidly rising".equals(baroTest.getDataTwo()));
       // update the data
       baroTest.updateData();
       // check again if data is within the range
-      assertTrue(16.00 <= Double.valueOf(baroTest.getDataOne()) 
-          && Double.valueOf(baroTest.getDataOne()) <= 32.50,"the Barometric data is not within the range");
-      assertTrue("Rapidly falling".equals(baroTest.getDataTwo())
+      assertTrue("the Barometric data is not within the range",
+                 16.00 <= Double.valueOf(baroTest.getDataOne()) 
+          && Double.valueOf(baroTest.getDataOne()) <= 32.50);
+      assertTrue("Barometric sensor cannot determines the rate of falling",
+                 "Rapidly falling".equals(baroTest.getDataTwo())
           || "Slowly falling".equals(baroTest.getDataTwo())
           || "Steady".equals(baroTest.getDataTwo()) 
           || "Slowly rising".equals(baroTest.getDataTwo())
-          || "Rapidly rising".equals(baroTest.getDataTwo()), "Barometric sensor cannot determines the rate of falling");
+          || "Rapidly rising".equals(baroTest.getDataTwo()));
     }
   }
 
